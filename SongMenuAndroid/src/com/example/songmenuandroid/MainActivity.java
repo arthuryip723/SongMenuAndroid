14c452b9fd48c67b56ca357ac58f7bac2b0456e6
Arthur Yip <arthuryip723@gmail.com>
2014-07-01 23:56:08 -0400

    20140701

--- SongMenuAndroid/src/com/example/songmenuandroid/MainActivity.java
+++ SongMenuAndroid/src/com/example/songmenuandroid/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.songmenuandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +22,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private void addListenerOnButton()
+	{
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// Implement the login function here
+				
+			}
+			
+		});
 	}
 
 }
