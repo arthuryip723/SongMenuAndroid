97402bf7072913a76e53da4153846b431f9b0e3a
Arthur Yip <arthuryip723@gmail.com>
2014-07-09 16:34:28 -0400

    20140709

--- SongMenuAndroid/src/com/example/songmenuandroid/MainActivity.java
+++ SongMenuAndroid/src/com/example/songmenuandroid/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.songmenuandroid;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +27,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		addListenerOnButton();
 	}
 
 	@Override
@@ -23,6 +37,10 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	EditText usernameET = (EditText)findViewById(R.id.editText1);
+	EditText passwordET = (EditText)findViewById(R.id.editText2);
+	String URL = "http://localhost/";
 	
 	private void addListenerOnButton()
 	{
@@ -33,7 +51,27 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				// Implement the login function here
-				
+				// Get the username, password
+				String username = usernameET.getText().toString();
+				String password = passwordET.getText().toString();
+				HttpClient httpClient = new DefaultHttpClient();
+				try {
+					HttpResponse response = httpClient.execute(new HttpGet(URL));
+					Log.d("Http Response:", response.toString());
+					HttpEntity entity = response.getEntity();
+					
+					if (entity != null) {
+						InputStream instream = entity.getContent();
+//						String result = convertStreamToString(instream);
+						instream.close();
+					}
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
 		});
